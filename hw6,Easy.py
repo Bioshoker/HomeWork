fd0ceb0e3285947957cdaa4a7d327eb8b31787ba
Bioshoker <108736082+Bioshoker@users.noreply.github.com>
2022-09-02 02:36:44 +0400

    Add files via upload

---
+++ hw6,Easy.py
@@ -0,0 +1,114 @@
+# Задача-1:
+# Следующая программа написана верно, однако содержит места потенциальных ошибок.
+# используя конструкцию try добавьте в код обработку соответствующих исключений.
+# Пример.
+# Исходная программа:
+def avg(a, b):
+    """Вернуть среднее геометрическое чисел 'a' и 'b'.
+
+    Параметры:
+        - a, b (int или float).
+
+    Результат:
+        - float.
+    """
+    return (a * b) ** 0.5
+
+
+a = float(input("a = "))
+b = float(input("b = "))
+c = avg(a, b)
+print("Среднее геометрическое = {:.2f}".format(c))
+
+
+# Решение
+
+def avg(a, b):
+    """Вернуть среднее геометрическое чисел 'a' и 'b'.
+
+    Параметры:
+        - a, b (int или float).
+
+    Результат:
+        - float.
+
+    Исключения:
+        - ValueError: вычисление не возможно.
+    """
+    if a * b >= 0:
+        return (a * b) ** 0.5
+    else:
+        raise ValueError("Невозможно определить среднее геометрическое "
+                         "введенных чисел.")
+
+
+try:
+    a = float(input("a = "))
+    b = float(input("b = "))
+    c = avg(a, b)
+    print("Среднее геометрическое = {:.2f}".format(c))
+except ValueError:
+    print("Пользоватеть введите только числа")
+
+# ПРИМЕЧАНИЕ: Для решения задач 2-4 необходимо познакомиться с модулями os, sys!
+# СМ.: https://pythonworld.ru/moduli/modul-os.html, https://pythonworld.ru/moduli/modul-sys.html
+
+# Задача-2:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+# из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
+
+import os
+
+def create_dirs ():
+    quantity_dirs = range(1,10)
+    for i in quantity_dirs:
+        i = str(i)
+        try:
+            os.makedirs('dir_'+i)
+        except FileExistsError:
+            print('dir_{} - уже существует'.format(i))
+
+def remove_dirs ():
+    quantity_dirs = range(1, 10)
+    for i in quantity_dirs:
+        i = str(i)
+        try:
+            os.removedirs('dir_' + i)
+        except FileNotFoundError:
+            print('dir_{} - папки не существует'.format(i))
+
+if __name__ == '__main__':
+    create_dirs()
+    remove_dirs()
+
+# Задача-3:
+# Напишите скрипт, отображающий папки текущей директории.
+
+def list_dir ():
+    buffer = os.listdir()
+    print('Список файлов:')
+    for index, element in enumerate(buffer, start=1):
+        if os.path.isdir(element):
+            print('{}. {}'.format(index, element))
+
+if __name__ == '__main__':
+    list_dir()
+
+# Задача-4:
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+import shutil
+
+
+def current_file_copy ():
+    name_file = os.path.realpath(__file__)
+    new_file = name_file +'.copy'
+    if os.path.isfile(new_file) != True:
+        shutil.copy(name_file, new_file)
+        return new_file + ' - создан'
+    else:
+        return 'Файл уже скопирован'
+
+if __name__ == '__main__':
+    print(current_file_copy())
